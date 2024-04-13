c38fb1d68a84209b09da5810fa4f83236f39f536
Robert Odhiambo <odhizrober254@gmail.com>
2024-04-13 19:15:10 +0300

    multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,8 @@ import { uploadPhoto, createUser } from './utils.js';
 
 function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then((userResponse) => {
-      console.log(`Body ${userResponse.firstName} ${userResponse.lastName}`);
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
     .catch((error) => {
       console.error('Signup system offline');
